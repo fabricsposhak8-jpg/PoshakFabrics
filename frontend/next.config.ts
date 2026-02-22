5df8b0a98cc30f0627e88677c247e2e940489ec7
AsadImran <iasad4855@gmail.com>
2026-02-22 07:48:20 +0500

    Removed backend node_modules from git

--- frontend/next.config.ts
+++ frontend/next.config.ts
@@ -1,7 +1,14 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
+  reactStrictMode: true,
+  images: {
+    domains: [
+      "poshak-fabrics.vercel.app", // your frontend domain
+      "localhost",                  // if you test locally
+      "your-backend-domain.com",    // replace with your backend if serving images from API
+    ],
+  },
 };
 
 export default nextConfig;
