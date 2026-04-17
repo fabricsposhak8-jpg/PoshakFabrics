80742666c6616d7f843410fe42234fd573b2dc37
AsadImran <iasad4855@gmail.com>
2026-04-17 10:25:22 +0500

    Chnages

--- Backend/controllers/cart_controller.js
+++ Backend/controllers/cart_controller.js
@@ -1,23 +1,30 @@
 import { addtoCart, getCart, removeFromCart, updateCartQuantity, clearCart } from "../models/cart_model.js";
-
+import redis from "../middlewares/Redis.js";
 export const AddToCartMiddleware = async (req, res, next) => {
     try {
         const userId = req.user.id;
         const { productId, quantity } = req.body;
         const result = await addtoCart(userId, productId, quantity);
-        res.status(200).json({ message: "Added to cart", result });
+        await redis.del("cart");
+        await redis.set("cart", result);
+        return res.status(200).json({ message: "Added to cart", result });
     } catch (error) {
-        res.status(500).json({ message: "Internal server error" });
+        return res.status(500).json({ message: "Internal server error" });
     }
 }
 
 export const GetCartMiddleware = async (req, res, next) => {
     try {
         const userId = req.user.id;
+        const cachedCart = await redis.get("cart");
+        if (cachedCart) {
+            return res.status(200).json({ message: "Cart", result: cachedCart });
+        }
         const result = await getCart(userId);
-        res.status(200).json({ message: "Cart", result });
+        await redis.set("cart", result);
+        return res.status(200).json({ message: "Cart", result });
     } catch (error) {
-        res.status(500).json({ message: "Internal server error" });
+        return res.status(500).json({ message: "Internal server error" });
     }
 }
 
@@ -25,10 +32,11 @@ export const RemoveFromCartMiddleware = async (req, res, next) => {
     try {
         const { productId } = req.body;
         const userId = req.user.id;
+        await redis.del(`cart:${userId}`);
         const result = await removeFromCart(userId, productId);
-        res.status(200).json({ message: "Removed from cart", result });
+        return res.status(200).json({ message: "Removed from cart", result });
     } catch (error) {
-        res.status(500).json({ message: "Internal server error" });
+        return res.status(500).json({ message: "Internal server error" });
     }
 }
 
@@ -36,19 +44,22 @@ export const UpdateQuantityMiddleware = async (req, res, next) => {
     try {
         const { productId, quantity } = req.body;
         const userId = req.user.id;
+        await redis.del(`cart:${userId}`);
         const result = await updateCartQuantity(userId, productId, quantity);
-        res.status(200).json({ message: "Quantity updated", result });
+        await redis.set(`cart:${userId}`, result);
+        return res.status(200).json({ message: "Quantity updated", result });
     } catch (error) {
-        res.status(500).json({ message: "Internal server error" });
+        return res.status(500).json({ message: "Internal server error" });
     }
 }
 
 export const ClearCartMiddleware = async (req, res, next) => {
     try {
         const userId = req.user.id;
+        await redis.del(`cart:${userId}`);
         const result = await clearCart(userId);
-        res.status(200).json({ message: "Cart cleared", result });
+        return res.status(200).json({ message: "Cart cleared", result });
     } catch (error) {
-        res.status(500).json({ message: "Internal server error" });
+        return res.status(500).json({ message: "Internal server error" });
     }
 }
